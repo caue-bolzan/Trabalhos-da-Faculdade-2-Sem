09718f1deb18f518a70a9f4eee7b78f728fb57a8
Cauê Bolzan <cauebolzan@gmail.com>
2025-08-15 19:03:40 +0000

    Revisao

--- Revisao/classificacao_estado_civil.c
+++ Revisao/classificacao_estado_civil.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+int main (){
+    char escolha[12];
+
+    scanf ("%s", &escolha[1]);
+
+    switch (escolha[1])
+    {
+    case 'c':
+        printf("Casado");
+        break;
+    
+    case 's':
+        printf("Solteiro");
+
+    case 'd':
+        printf("Divorciado");
+    
+    default:
+        printf("Alguma letra errado")
+        break;
+    }
+
+
+    return 0;
+}
